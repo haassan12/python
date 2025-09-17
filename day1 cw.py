79b2e9b35b199885936d42938750300537c047f9
haassan12 <haassan369@gmail.com>
2025-09-17 23:31:38 +0530

    Add implementation for calculating total volume with random bonus

---
+++ day1 cw.py
@@ -0,0 +1,10 @@
+import random
+apple=15.5
+orange=20
+grape=10.25
+volume=apple+orange+grape
+print(f"total volume is:{volume}")
+print(f"totall volume in int is:{int(volume)}")
+print(f"total volume in string is:{str(volume)}")
+bonus=random.randint(5,10)
+print(f"total volume with bonus is:{volume+bonus}")
